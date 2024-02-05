5b6ac068eb737c622d9b547a567fbbefb663315e
Melody Lee <melody.lee357@gmail.com>
2024-02-05 08:54:49 -0500

    added functioning listview

--- app/src/main/java/com/example/backup_college_scheduler/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/backup_college_scheduler/ui/dashboard/DashboardFragment.java
@@ -24,7 +24,7 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
+        final TextView textView = binding.assignmentEnterName;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
