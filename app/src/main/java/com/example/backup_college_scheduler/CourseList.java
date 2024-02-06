d9be552561df0b0688e6644ba3a1f2fdbf6b1fa6
Melody Lee <melody.lee357@gmail.com>
2024-02-05 20:04:02 -0500

    UI structure

--- app/src/main/java/com/example/backup_college_scheduler/CourseList.java
+++ app/src/main/java/com/example/backup_college_scheduler/CourseList.java
@@ -12,6 +12,7 @@ public class CourseList {
     private static ArrayList<Course> list = new ArrayList<>();
 
     public static ArrayList<Course> getCourseData() {
+        list.clear();
         list.add(new Course("CS1332", "Data structures & algorithms", "Faulkner", "MWF 3:30pm-4:20pm"));
         list.add(new Course("MATH2106", "Foundations of math proofs", "Shahaf", "TTh 12:30pm-1:20pm"));
         list.add(new Course("CS2340", "Objects & design", "Pedro", "MW 8:00am-9:15am"));
--- app/src/main/java/com/example/backup_college_scheduler/ExamList.java
+++ app/src/main/java/com/example/backup_college_scheduler/ExamList.java
@@ -6,6 +6,7 @@ public class ExamList {
     private static ArrayList<Exam> list = new ArrayList<>();
 
     public static ArrayList<Exam> getExamData() {
+        list.clear();
         list.add(new Exam("Midterm 1", "ADTs", "CS1332", "CULC 154", "2024:02:08 3:30pm-4:20pm"));
         return list;
     }
